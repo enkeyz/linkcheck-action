946412f5bdb0f2ef9339fe0e805a47ae36a6c91c
Norbert Klar <klarnorbert@gmail.com>
2022-08-26 14:12:37 +0200

    making servers happy

--- pkg/link/health.go
+++ pkg/link/health.go
@@ -8,8 +8,8 @@ import (
 
 func CheckHealth(ctx context.Context, url string) error {
 	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
-	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
+	req.Header.Add("Accept", `text/html`)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36`)
 	req = req.WithContext(ctx)
 
 	res, err := http.DefaultClient.Do(req)
