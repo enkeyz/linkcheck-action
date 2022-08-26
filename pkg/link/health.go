b24a5a90290b3c4ab0f78c5310724b3395ff0a5d
Norbert Klar <klarnorbert@gmail.com>
2022-08-26 14:04:19 +0200

    added request headers to make webservers happy

--- pkg/link/health.go
+++ pkg/link/health.go
@@ -8,6 +8,8 @@ import (
 
 func CheckHealth(ctx context.Context, url string) error {
 	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
 	req = req.WithContext(ctx)
 
 	res, err := http.DefaultClient.Do(req)
